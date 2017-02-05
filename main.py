9a037ee5f445b8b3afef6177250a2c6f76f97dbd
Mrinmoy Biswas Akash <skydelta71@gmail.com>
2017-02-06 03:58:34 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+'''
+Picohash
+A python program that converts images into ASCII graphics
+'''
+
+import os
+from PIL import Image
+
+imageList = [i for i in os.listdir('.') if i.endswith('.jpg') or i.endswith('.png') or i.endswith('.jpeg')]
+imageList.sort()
+print imageList
+
+#mapping
+pixelMap = { 'E1':' ', 'C5':'.', 'A9':',', '8C':"-", '70':"'", '56':':', '3B':';', '22':'+', '00':'#'}
+
+
+def modification(imagefile):
+	text = open(imagefile.split('.')[0]+'.html', 'w')
+	text.write('<!DOCTYPE html><html><head><title>'+imagefile+'</title><style>body {font-size: 3px;}</style></head><body><pre>')
+	
+	image = Image.open(imagefile).convert('L')
+	print 'processing ' + imagefile
+	rgbim = image.convert('RGB')
+	w, h = rgbim.size
+
+	for i in xrange(w):
+		for j in xrange(h):
+			px = rgbim.getpixel((i, j))
+			for sym in ['E1', 'C5', 'A9', '8C', '70', '56', '3B', '22', '00']:
+				symVal = int(sym, 16)
+				if(px >= (symVal, symVal, symVal)):
+					text.write(pixelMap[sym])
+					break
+		text.write('\n')
+			
+	text.write('</pre></body></html>')
+			
+	text.close()
+	image.close()
+	rgbim.close()
+
+for imagefile in imageList:
+	print 'Do you want to process ' + imagefile + '? [y/n]'
+	inp = raw_input()
+	if inp=='y' or inp=='Y':
+		modification(imagefile)
+	else:
+		continue
